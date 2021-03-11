67f85f8de668554c1c6469748de05c78a5450d15
Will Papp <wpapp90@gmail.com>
2021-03-11 15:28:04 -0600

    added manager class and changed paramaters to match promt names

--- index.js
+++ index.js
@@ -1,28 +1,31 @@
+//Global Variables
 const fs = require('fs');
-
 const inquirer = require('inquirer');
+const teamArray = [];
+console.log(teamArray)
 
+//Init function with inquirer prompt to get info
 function init() {
     inquirer.prompt([
         {
             type: 'input',
             message: 'Please enter team manager name.',
-            name: 'manName',
+            name: 'name',
         },
         {
            type: 'input',
            message: 'Please enter team manager employee ID.', 
-           name: 'manID'
+           name: 'ID'
         },
         {
             type: 'input',
             message: 'Please enter team manager email adress.',
-            name: 'manEmail',
+            name: 'email',
         },
         {
             type: 'input',
             message: 'Please enter team manager office number.',
-            name: 'manOfficeNum',
+            name: 'office',
         },
         {
             type: 'list',
@@ -30,54 +33,71 @@ function init() {
             name: 'addMember',
             choices: ['Add an Engineer', 'Add an Intern', 'Finish building team']
         },
+    //Then function manipulating the response
     ]).then(response => {
         console.log(response)
+        //First option to finish building team
         if (response.addMember === 'Finish building team') {
-            console.log('team finished');
+            // const addManager = new Manager (response.manName, response.manID, response. manEmail, response.manOfficeNum);
+            // teamArray.push(addManager);
         } else if (response.addMember === 'Add an Engineer') {
             inquirer.prompt([
                 {
                    type: 'input',
                    message: 'Please enter Engeneer name.',
-                   name: 'engName', 
+                   name: 'name', 
                 },
                 {
                     type: 'input',
                     message: 'Please enter Engeneer ID.',
-                    name: 'engID',
+                    name: 'ID',
                 },
                 {
                     type: 'input',
                     message: 'Please enter Engeneer GitHub username.',
-                    name: 'engGit',
+                    name: 'Git',
                 },
             ])
+        //Add Intern prompt
         } else if (response.addMember === 'Add an Intern') {
             inquirer.prompt([
                 {
                     type: 'input',
                     message: 'Please enter Intern name.',
-                    name: 'internName',
+                    name: 'Name',
                 },
                 {
                     type: 'input',
                     message: 'Please enter Intern ID.',
-                    name: 'internID',
+                    name: 'ID',
                 },
                 {
                     type: 'input',
                     message: 'Please enter intern email.',
-                    name: 'internEmail',
+                    name: 'email',
                 },
                 {
                     type: 'input',
                     message: 'Please enter Intern School.',
-                    name: 'internSchool',
+                    name: 'school',
                 },
             ])
         }
     })
 }
 
+function additionalMem() {
+    inquirer.prompt([
+        {
+            type: 'confirm',
+            message: 'Would you like to add another team member of the same position?',
+            name: 'additionalMember',
+        }        
+    ]).then(response => {
+        if (response === true) {
+
+        }
+    })
+}
 init();
 
